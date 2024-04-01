06765a493127ae349d180c4948659fabd5e5a4fb
Tchikaya Ariel <tchikayaline@gmail.com>
2024-04-01 13:27:32 +0100

    <feat> latest update

--- vite.config.ts
+++ vite.config.ts
@@ -22,6 +22,31 @@ export default defineConfig({
                 theme_color: '#becdaf',
                 icons: [
                   {
+                    src: 'logo36.png',
+                    sizes: '36x36',
+                    type: 'image/png'
+                  },
+                  {
+                    src: 'logo48.png',
+                    sizes: '48x48',
+                    type: 'image/png'
+                  },
+                  {
+                    src: 'logo72.png',
+                    sizes: '72x72',
+                    type: 'image/png'
+                  },
+                  {
+                    src: 'logo96.png',
+                    sizes: '96x96',
+                    type: 'image/png'
+                  },
+                  {
+                    src: 'logo144.png',
+                    sizes: '144x144',
+                    type: 'image/png'
+                  },
+                  {
                     src: 'logo192.png',
                     sizes: '192x192',
                     type: 'image/png'
@@ -29,6 +54,11 @@ export default defineConfig({
                   {
                     src: 'logo256.png',
                     sizes: '256x256',
+                    type: 'image/png'
+                  },
+                  {
+                    src: 'logo384.png',
+                    sizes: '384x384',
                     type: 'image/png'
                   },
                   {
