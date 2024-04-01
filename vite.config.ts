883a5082e64cf5d722500cc993650e77cba74da7
Tchikaya Ariel <tchikayaline@gmail.com>
2024-04-01 08:35:59 +0100

    <feat> latest update

--- vite.config.ts
+++ vite.config.ts
@@ -22,55 +22,14 @@ export default defineConfig({
                 theme_color: '#becdaf',
                 icons: [
                   {
-                    src: 'logo36.png',
-                    sizes: '36x36',
-                    type: 'image/png'
-                  },
-                  {
-                    src: 'logo48.png',
-                    sizes: '48x48',
-                    type: 'image/png'
-                  },
-                  {
-                    src: 'logo72.png',
-                    sizes: '72x72',
-                    type: 'image/png'
-                  },
-                  {
-                    src: 'logo96.png',
-                    sizes: '96x96',
-                    type: 'image/png'
-                  },
-                  {
-                    src: 'logo144.png',
-                    sizes: '144x144',
-                    type: 'image/png'
+                    src: 'logo192.png',
+                    sizes: '192x192',
+                    type: 'image/svg'
                   },
                   {
                     src: 'logo192.png',
                     sizes: '192x192',
-                    type: 'image/png'
-                  },
-                  {
-                    src: 'logo256.png',
-                    sizes: '256x256',
-                    type: 'image/png'
-                  },
-                  {
-                    src: 'logo384.png',
-                    sizes: '384x384',
-                    type: 'image/png'
-                  },
-                  {
-                    src: 'logo512.png',
-                    sizes: '512x512',
-                    type: 'image/png'
-                  },
-                  {
-                    src: 'maskable_icon.png',
-                    sizes: '259x259',
-                    type: 'image/png',
-                    purpose: 'maskable'
+                    type: 'image/svg'
                   }
                 ],
                 screenshots: [
